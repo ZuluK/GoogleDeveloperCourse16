9278f7afbc011c91083671a43680283b5c344f28
Richard Spence <richardspence@Richards-MacBook-Air-2.local>
2018-02-06 14:31:06 -0800

    First Commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+
+
+//Persistent data structures for efficient immutablity
+
+/*
+ * Programming Quiz: Murder Mystery (3-4)
+ */
+
+// change the value of `room` and `suspect` to test your code
+var room = "ballroom";
+var suspect = "Mr. Parkes";
+
+var weapon = "";
+var solved = false;
+
+if (room === "dining room") {
+    weapon = "knife";
+    solved = true;
+    suspect = "Mr. Parkes";
+} else if (room === "gallery") {
+    weapon = "trophy"
+    solved = true;
+    suspect = "Ms. Van Cleve";
+} else if (room == "ballroom") {
+    weapon = "poison";
+    solved = true;
+    suspect = "Mr. Kalehoff";
+} else {
+    weapon = "pool stick";
+    solved = true;
+    suspect = "Mrs. Sparr";
+
+}
+
+if (solved) {
+	console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
+}
+/*
+ * Programming Quiz: Laugh it Off 2 (5-2)
+ *
+ * Write a function called `laugh` with a parameter named `num` that represents the number of "ha"s to return.
+ *
+ * Note:
+ *  - make sure your the final character is an exclamation mark ("!")
+ *  - make sure that your function produces the correct results when it is called multiple times
+ */
+
+var laugh = function(num){
+    var phrase = "";
+    for(var i = 0; i < num; i++){
+      //var i is 0; 0 is less than num(20);
+      //if so "ha" is added to the empty string
+      //var i is increased by one, and return to the begin of loop
+      //var is now 1; 1 is less than num(20);
+      //"ha" is added to the string
+      //var is increased by one
+        phrase += "ha";
+    }
+    return phrase + "!";
+}
+console.log(laugh(20));
